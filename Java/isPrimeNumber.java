811c8632e2a2d7b8fefb12754a4bb96891732587
Jasmine Roebuck <35498193+roebuckj3@users.noreply.github.com>
2018-04-30 08:56:30 -0600

    Add files via upload

---
+++ Java/isPrimeNumber.java
@@ -0,0 +1,14 @@
+public boolean isPrimeNumber(int n) {
+    if (n <= 1) {
+        return false;
+    } 
+    for (int i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
