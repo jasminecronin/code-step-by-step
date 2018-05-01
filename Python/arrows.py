4d446b224a8f5bb747f9df6fec37b9c984b13627
Jasmine Roebuck <35498193+roebuckj3@users.noreply.github.com>
2018-05-01 08:48:52 -0600

    Create arrows.py

---
+++ Python/arrows.py
@@ -0,0 +1,22 @@
+def main():
+    print1()
+    print1()
+    print2()
+    print1()
+    print3()
+    print1()
+    
+def print1():
+    print(" /\\")
+    print("/  \\")
+    print()
+
+def print2():
+    print(" ||")
+    print(" ||")
+    
+def print3():
+    print("\\  /")
+    print(" \\/")
+
+main()
