22759eb63ab4a33fb2476cf8f74c3e45747bbe39
Jasmine Roebuck <35498193+roebuckj3@users.noreply.github.com>
2018-05-05 10:38:12 -0600

    Create inches_to_centimeters.py

---
+++ Python/inches_to_centimeters.py
@@ -0,0 +1,4 @@
+print("This program converts feet and inches to centimeters.")
+f = int(input("Enter number of feet: "))
+i = int(input("Enter number of inches: "))
+print(f, "ft", i, "in =", ((f * 12) + i) * 2.54, "cm")
