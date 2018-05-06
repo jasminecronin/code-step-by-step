cc75e3c3bbdca473a0ab609d8314fa6aec17bd96
Jasmine Roebuck <35498193+roebuckj3@users.noreply.github.com>
2018-05-06 07:36:32 -0600

    Create receipt.py

---
+++ Python/receipt.py
@@ -0,0 +1,10 @@
+def main():
+    subtotal = 38 + 40 + 30
+    tax = subtotal * 0.08
+    tip = subtotal * 0.15
+    print("Subtotal:", subtotal)
+    print("Tax:", tax)
+    print("Tip:", tip)
+    print("Total:", subtotal + tax + tip)
+
+main()
