62171087efdea76be1a0e733cc64f6605b3e5d91
Jasmine Roebuck <35498193+roebuckj3@users.noreply.github.com>
2018-05-06 07:41:23 -0600

    Create stanford_vs_cal.py

---
+++ Python/stanford_vs_cal.py
@@ -0,0 +1,6 @@
+stan = int(input("Stanford: How many points did they score? "))
+cal = int(input("Cal: How many points did they score? "))
+if stan > cal:
+    print("Stanford won!")
+else:
+    print("Cal won!")
