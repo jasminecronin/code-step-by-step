3dd58d3b581644ef0fe5e8ac162c7819403dbec0
Jasmine Roebuck <35498193+roebuckj3@users.noreply.github.com>
2018-05-06 07:45:59 -0600

    Create towers.py

---
+++ Python/towers.py
@@ -0,0 +1,27 @@
+def main():
+    a()
+    b()
+    c()
+    b()
+    print()
+    a()
+    b()
+    c()
+    print()
+    a()
+    c()
+
+def a():
+    print("  *")
+    print(" ***")
+    print("*****")
+    
+def b():
+    print("-----")
+    print("-----")
+    
+def c():
+    print(" ###")
+    print(" ###")
+    
+main()
